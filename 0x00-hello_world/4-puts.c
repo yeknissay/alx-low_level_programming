c0c2e6af6d1d59ecae9a9932c3f305795b0f9e6c
yeknissay <karouach.yassine@gmail.com>
2023-02-20 15:45:55 -0800

    correct1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,18 +3,11 @@
 /**
  * main - Entry point
  *
- * Author: Sammykingx
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char string[70] = "\"Programming is like building a multilingual puzzle\"";
-	
-	puts(string);
-
-	/* Tried printing it directly */
-
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
